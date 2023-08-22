b2fc7da90c25d37e841311bd6dc6879d1f85630a
Daeng Deni Mardaeni <daeng.deni@dharma.or.id>
2023-08-22 13:25:45 +0700

    add script list file in directory

---
+++ list-file-directory.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Specify the directory path
+directory="/path/to/directory"
+
+# Use a loop to iterate over the files in the directory
+for file in "$directory"/*; do
+    # Check if the item is a file (not a directory)
+    if [[ -f "$file" ]]; then
+        # Extract and print the file name
+        filename=$(basename "$file")
+        echo "$filename"
+    fi
+done
